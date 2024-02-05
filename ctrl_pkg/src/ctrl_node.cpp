ad4c496a4d84b4b349e041a0cf133fbd3b7d6780
sahgautam <gshah26688@gmail.com>
2024-02-05 21:31:17 +0545

    pushing some more code

--- ctrl_pkg/src/ctrl_node.cpp
+++ ctrl_pkg/src/ctrl_node.cpp
@@ -431,13 +431,4 @@ namespace SysCtrl {
 }
 
 
-int main(int argc, char * argv[])
-{
-    rclcpp::init(argc, argv);
-    auto node = std::make_shared<SysCtrl::CtrlNodeMgr>("control_node");
-    rclcpp::executors::MultiThreadedExecutor exec;
-    exec.add_node(node);
-    exec.spin();
-    rclcpp::shutdown();
-    return 0;
-}
+
